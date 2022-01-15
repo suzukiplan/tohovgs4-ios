710fb6cf6ba59314035e01b72a109bda440ebf3e
suzukiplan <suzukiplan.ys@gmail.com>
2022-01-15 11:47:06 +0900

    Androidからcompatソースを持ってくる

---
+++ TOHOVGS/vgs/compat/vge.h
@@ -0,0 +1,126 @@
+/**
+ * ©2022, SUZUKI PLAN
+ * License: https://github.com/suzukiplan/tohovgs4-android/blob/master/LICENSE.txt
+ */
+#ifndef _INCLUDE_VGE_H
+#define _INCLUDE_VGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAXSLOT 2
+#define XSIZE 240
+#define YSIZE 320
+
+struct _VRAM {
+    unsigned int pal[256];     /* パレット領域 */
+    unsigned char sp[0x20000]; /* スプライト領域 */
+};
+
+struct _SLOT {
+    int xs;
+    int ys;
+    unsigned char dat[65536];
+};
+
+struct _TOUCH {
+    int s;  /* 1:タッチ中, 0:リリース中 */
+    int dx; /* X方向移動ドット数 */
+    int dy; /* Y方向移動ドット数 */
+    int cx; /* 現在のX座標 */
+    int cy; /* 現在のY座標 */
+    int px; /* 直前のX座標 */
+    int py; /* 直前のY座標 */
+};
+
+struct Preferences {
+    int currentTitleId;
+    int base;
+    int infinity;
+    int loop;
+    int kobushi;
+    int localeId;
+    int listType;
+};
+
+extern struct _VRAM _vram;
+extern struct _SLOT _slot[MAXSLOT];
+extern struct _TOUCH _touch;
+extern short *TONE1[85];
+extern short *TONE2[85];
+extern short *TONE3[85];
+extern short *TONE4[85];
+extern void *_psg;
+extern int g_flingY;
+extern int g_flingX;
+
+void tohovgs_cleanUp(void);
+
+void tohovgs_allocate(int nTitle, int nSong);
+
+void tohovgs_loadKanji(const void *data, size_t size);
+
+void tohovgs_setTitle(int index,
+                      int id,
+                      int songNum,
+                      void *title,
+                      size_t titleSize,
+                      void *copyright,
+                      size_t copyrightSize);
+
+void tohovgs_setSong(int index,
+                     int id,
+                     int no,
+                     int loop,
+                     int col,
+                     void *mmlPath,
+                     size_t mmlPathSize,
+                     void *titleJ,
+                     size_t titleSizeJ,
+                     void *titleE,
+                     size_t titleSizeE);
+
+void tohovgs_setPreference(int currentTitleId,
+                           int loop,
+                           int base,
+                           int infinity,
+                           int kobushi,
+                           int localeId,
+                           int listType);
+
+struct Preferences *tohovgs_getPreference(void);
+
+int vge_tick(void);
+
+int vge_gload(unsigned char n, const unsigned char *bin);
+
+void vge_putSP(unsigned char n, int sx, int sy, int xs, int ys, int dx, int dy);
+
+void vge_putSPM(unsigned char n, int sx, int sy, int xs, int ys, int dx, int dy, unsigned char c);
+
+void vge_pixelSP(int x, int y, unsigned char c);
+
+void vge_lineSP(int fx, int fy, int tx, int ty, unsigned char c);
+
+void vge_boxSP(int fx, int fy, int tx, int ty, unsigned char c);
+
+void vge_boxfSP(int fx, int fy, int tx, int ty, unsigned char c);
+
+void vge_touch(int *s, int *cx, int *cy, int *dx, int *dy);
+
+void vge_bplay(const char *mmlPath);
+
+void vge_bstop(void);
+
+void vge_bresume(void);
+
+void vge_restartCurrentSong(void);
+
+void vgsbuf(char *buf, size_t size);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
---
+++ TOHOVGS/vgs/compat/vgeapi.c
@@ -0,0 +1,281 @@
+/*
+ * Copyright 2012, Suzuki Plan.
+ * License: https://github.com/suzukiplan/tohovgs4-android/blob/master/LICENSE.txt
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vge.h"
+#include "vgsdec.h"
+#include "vgsmml.h"
+
+/* グローバル変数の実体宣言 */
+struct _VRAM _vram;
+struct _SLOT _slot[MAXSLOT];
+struct _TOUCH _touch;
+void *_psg;
+static int fs_bstop;
+
+/* 内部関数 */
+static int gclip(unsigned char n, int *sx, int *sy, int *xs, int *ys, int *dx, int *dy);
+
+static void pixel(unsigned char *p, int x, int y, unsigned char c);
+
+static void line(unsigned char *p, int fx, int fy, int tx, int ty, unsigned char c);
+
+static void boxf(unsigned char *p, int fx, int fy, int tx, int ty, unsigned char c);
+
+int vge_gload(unsigned char n, const unsigned char *bin) {
+    int gSize;
+    int i;
+    if ('S' != bin[0] || 'Z' != bin[1]) {
+        return -1;
+    }
+    _slot[n].xs = bin[2] + 1;
+    _slot[n].ys = bin[3] + 1;
+    gSize = (_slot[n].xs) * (_slot[n].ys);
+
+    /* パレット情報を読み込む */
+    bin += 4;
+    memcpy(_vram.pal, bin, sizeof(_vram.pal));
+
+    /* パレットを ARGB8888 -> RGB565 に変換 */
+    for (i = 0; i < 256; i++) {
+        unsigned int r = (_vram.pal[i] & 0x00F80000) >> 8;
+        unsigned int g = (_vram.pal[i] & 0x0000FC00) >> 5;
+        unsigned int b = (_vram.pal[i] & 0x000000F8) >> 3;
+        _vram.pal[i] = r | g | b;
+    }
+
+    /* 画像データを読み込む */
+    bin += sizeof(_vram.pal);
+    memcpy(_slot[n].dat, bin, gSize);
+
+    return 0;
+}
+
+void vge_putSP(unsigned char n, int sx, int sy, int xs, int ys, int dx, int dy) {
+    int i, j;
+    int posT;
+    int posF;
+    if (gclip(n, &sx, &sy, &xs, &ys, &dx, &dy)) {
+        return;
+    }
+    /* 透明色以外のピクセルを1ピクセルづつ設定 */
+    posT = dy * XSIZE + dx;
+    posF = sy * _slot[n].xs + sx;
+    for (j = 0; j < ys; j++) {
+        for (i = 0; i < xs; i++) {
+            if (_slot[n].dat[posF]) {
+                _vram.sp[(posT & 0x1ffff)] = _slot[n].dat[posF];
+            }
+            posT++;
+            posF++;
+        }
+        posT += XSIZE - xs;
+        posF += _slot[n].xs - xs;
+    }
+}
+
+void vge_putSPM(unsigned char n, int sx, int sy, int xs, int ys, int dx, int dy, unsigned char c) {
+    int i, j;
+    int posT;
+    int posF;
+    if (gclip(n, &sx, &sy, &xs, &ys, &dx, &dy)) {
+        return;
+    }
+    /* 透明色以外のピクセルを1ピクセルづつ設定 */
+    posT = dy * XSIZE + dx;
+    posF = sy * _slot[n].xs + sx;
+    for (j = 0; j < ys; j++) {
+        for (i = 0; i < xs; i++) {
+            if (_slot[n].dat[posF]) {
+                _vram.sp[(posT & 0x1ffff)] = c;
+            }
+            posT++;
+            posF++;
+        }
+        posT += XSIZE - xs;
+        posF += _slot[n].xs - xs;
+    }
+}
+
+static int gclip(unsigned char n, int *sx, int *sy, int *xs, int *ys, int *dx, int *dy) {
+    /* モロにはみだしてないか？ */
+    if ((*sx) < 0 || _slot[n].xs < (*sx) + (*xs) || (*sy) < 0 || _slot[n].ys < (*sy) + (*ys) ||
+        (*dx) + (*xs) < 0 || XSIZE <= *dx || (*dy) + (*ys) < 0 ||
+        YSIZE <= *dy) {
+        return -1; /* モロはみだし刑事 */
+    }
+    /* 左側のクリッピング処理  */
+    if ((*dx) < 0) {
+        (*sx) -= (*dx);
+        (*xs) += (*dx);
+        (*dx) = 0;
+    }
+    /* 右側のクリッピング処理  */
+    if (XSIZE < (*dx) + (*xs)) {
+        (*xs) -= ((*dx) + (*xs)) - XSIZE;
+    }
+    /* 上側のクリッピング処理  */
+    if ((*dy) < 0) {
+        (*sy) -= (*dy);
+        (*ys) += (*dy);
+        (*dy) = 0;
+    }
+    /* 下側のクリッピング処理  */
+    if (YSIZE < (*dy) + (*ys)) {
+        (*ys) -= ((*dy) + (*ys)) - YSIZE;
+    }
+    return 0;
+}
+
+void vge_pixelSP(int x, int y, unsigned char c) {
+    pixel(_vram.sp, x, y, c);
+}
+
+static inline void pixel(unsigned char *p, int x, int y, unsigned char c) {
+    if (0 <= x && x < XSIZE && 0 <= y && y < YSIZE) {
+        p[y * XSIZE + x] = c;
+    }
+}
+
+void vge_lineSP(int fx, int fy, int tx, int ty, unsigned char c) {
+    line(_vram.sp, fx, fy, tx, ty, c);
+}
+
+static inline void line(unsigned char *p, int fx, int fy, int tx, int ty, unsigned char c) {
+    int idx, idy;
+    int w;
+    idx = tx - fx;
+    idy = ty - fy;
+    if (!idx || !idy) {
+        /* form -> to変換 */
+        if (tx < fx) {
+            w = fx;
+            fx = tx;
+            tx = w;
+        }
+        if (ty < fy) {
+            w = fy;
+            fy = ty;
+            ty = w;
+        }
+        if (0 == idy) {
+            /* 水平線(Y方向増分なし) ... 最も高速な線描画 */
+            for (; fx <= tx; fx++) {
+                pixel(p, fx, fy, c);
+            }
+        } else {
+            /* 垂直線(X方向増分なし) ... 二番目に高速な線描画 */
+            for (; fy <= ty; fy++) {
+                pixel(p, fx, fy, c);
+            }
+        }
+        return;
+    }
+}
+
+void vge_boxSP(int fx, int fy, int tx, int ty, unsigned char c) {
+    vge_lineSP(fx, fy, tx, fy, c);
+    vge_lineSP(fx, ty, tx, ty, c);
+    vge_lineSP(fx, fy, fx, ty, c);
+    vge_lineSP(tx, fy, tx, ty, c);
+}
+
+void vge_boxfSP(int fx, int fy, int tx, int ty, unsigned char c) {
+    boxf(_vram.sp, fx, fy, tx, ty, c);
+}
+
+static inline void boxf(unsigned char *p, int fx, int fy, int tx, int ty, unsigned char c) {
+    int w;
+    /* form -> to変換 */
+    if (tx < fx) {
+        w = fx;
+        fx = tx;
+        tx = w;
+    }
+    if (ty < fy) {
+        w = fy;
+        fy = ty;
+        ty = w;
+    }
+    /* 範囲外描画の抑止 */
+    if (XSIZE <= fx || YSIZE <= fy || tx < 0 || ty < 0) {
+        return;
+    }
+    /* クリッピング */
+    if (fx < 0) {
+        fx = 0;
+    }
+    if (fy < 0) {
+        fy = 0;
+    }
+    if (XSIZE <= tx) {
+        tx = XSIZE - 1;
+    }
+    if (YSIZE <= ty) {
+        ty = YSIZE - 1;
+    }
+    /* Xの描画サイズを予め求めておく */
+    w = tx - fx;
+    w++;
+    /* 描画 */
+    for (; fy <= ty; fy++) {
+        memset(&p[fy * XSIZE + fx], c, w);
+    }
+}
+
+void vge_touch(int *s, int *cx, int *cy, int *dx, int *dy) {
+    *s = _touch.s;
+    *cx = _touch.cx;
+    *cy = _touch.cy;
+    *dx = _touch.dx;
+    *dy = _touch.dy;
+    _touch.px = _touch.cx;
+    _touch.py = _touch.cy;
+    _touch.dx = 0;
+    _touch.dy = 0;
+}
+
+void vge_bplay(const char *mmlPath) {
+    struct VgsMmlErrorInfo err;
+    struct VgsBgmData *data;
+    static unsigned char mmlDataBuffer[1048576];
+    int mmlSize = 0;
+    int readSize;
+    memset(mmlDataBuffer, 0, sizeof(mmlDataBuffer));
+    FILE *fp = fopen(mmlPath, "rb");
+    if (!fp) return;
+    do {
+        readSize = (int)fread(&mmlDataBuffer[mmlSize], 1, 1024, fp);
+        mmlSize += readSize;
+    } while (0 < readSize);
+    mmlSize++;
+    fclose(fp);
+    data = vgsmml_compile_from_memory(mmlDataBuffer, mmlSize, &err);
+    vgsdec_load_bgm_from_memory(_psg, data->data, data->size);
+    vgsmml_free_bgm_data(data);
+    vgsdec_set_value(_psg, VGSDEC_REG_RESET, 1);
+    vgsdec_set_value(_psg, VGSDEC_REG_SYNTHESIS_BUFFER, 1);
+    vgsdec_set_value(_psg, VGSDEC_REG_TIME, 0);
+    fs_bstop = 0;
+}
+
+void vge_bstop(void) {
+    fs_bstop = 1;
+}
+
+void vge_bresume(void) {
+    fs_bstop = 0;
+}
+
+void vge_restartCurrentSong(void) {
+    vgsdec_set_value(_psg, VGSDEC_REG_TIME, 0);
+}
+
+void vgsbuf(char *buf, size_t size) {
+    memset(buf, 0, size);
+    if (fs_bstop) return;
+    vgsdec_execute(_psg, buf, size);
+}
